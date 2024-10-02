1c4e91f4a248af97408aad9728d9c6c726fd5bb7
mvpkenlin <student-03-4ebe67fd1eba@qwiklabs.net>
2024-10-02 08:04:56 +0000

    update version to 0.0.1

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app_version = "0.0.0"
+app_version = "0.0.1"
 
 @app.route("/")
 def hello_world():
@@ -13,4 +13,3 @@ def hello_world():
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
-    
